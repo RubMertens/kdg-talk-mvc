abeee3f21e3dff3502a064e6ea36550788e5a201
Ruben Mertens <=>
2021-03-12 14:49:26 +0100

    editor for taghelper

--- VotingAppStart/Voting.WebApp/CustomTagHelpers/EditorForTagHelper.cs
+++ VotingAppStart/Voting.WebApp/CustomTagHelpers/EditorForTagHelper.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Voting.WebApp.CustomTagHelpers
 {
@@ -23,6 +27,46 @@ namespace Voting.WebApp.CustomTagHelpers
             output.TagName = "";
             ((IViewContextAware) htmlHelper).Contextualize(ViewContext);
             output.Content.SetHtmlContent(htmlHelper.EditorForModel());
+        }
+    }
+
+    [HtmlTargetElement("editor-for")]
+    public class EditorTagHelper : TagHelper
+    {
+        private readonly IHtmlHelper htmlHelper;
+
+        public ModelExpression ModelExpression { get; set; }
+        public string Template { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public EditorTagHelper(IHtmlHelper htmlHelper)
+        {
+            this.htmlHelper = htmlHelper;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            if (!output.Attributes.ContainsName(nameof(Template)))
+            {
+                output.Attributes.Add(nameof(Template), Template);
+            }
+
+            output.SuppressOutput();
+
+            (htmlHelper as IViewContextAware)?.Contextualize(ViewContext);
+
+            output.Content.SetHtmlContent(htmlHelper.Editor(ModelExpression.Name, Template));
+
+            await Task.CompletedTask;
         }
     }
 }
